baa137f0ef222337b98dee767a0d067461363563
Bishal Prasad <bishal.prasad3787@gmail.com>
2024-01-23 23:30:56 +0530

    Added Project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
+import "../src/index.css";
+import App from "./components/App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
