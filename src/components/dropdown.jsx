d6c0879c4b77713b2c44aa0557853c74875a02f4
Bishal Prasad <bishal.prasad3787@gmail.com>
2024-02-07 15:20:49 +0530

    Updated

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,85 +1,85 @@
 import React from 'react'
 
-const dropdown = () => {
+const Dropdown = () => {
   return (
-    <div class="hidden lg:block h-full dark:text-white">
-    <div class="relative h-full flex items-center" data-headlessui-state="open">
-        <button class="ml-[1.375rem]" type="button" aria-expanded="true" data-headlessui-state="open" id="headlessui-popover-button-:Rv696:" aria-controls="headlessui-popover-panel-:r28:">
-            <span class="w-6 h-6 dark:text-white">
+    <div className="hidden lg:block h-full dark:text-white">
+    <div className="relative h-full flex items-center" data-headlessui-state="open">
+        <button className="ml-[1.375rem]" type="button" aria-expanded="true" data-headlessui-state="open" id="headlessui-popover-button-:Rv696:" aria-controls="headlessui-popover-panel-:r28:">
+            <span className="w-6 h-6 dark:text-white">
                 <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                    <path d="M3.75 12.1211H20.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M3.75 6.12109H20.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M3.75 18.1211H20.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    <path d="M3.75 12.1211H20.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M3.75 6.12109H20.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M3.75 18.1211H20.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                 </span>
             </span>
         </button>
         <button type="button" aria-hidden="true" style="position: fixed; top: 1px; left: 1px; width: 1px; height: 0px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; border-width: 0px;"></button>
         <button type="button" aria-hidden="true" style="position: fixed; top: 1px; left: 1px; width: 1px; height: 0px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; border-width: 0px;"></button>
-        <div class="bg-white border-wynk-borderWhite z-30 absolute top-full mt-2 w-64 h-fit dark:bg-wynk-HeaderPopDark -bottom-[152px] right-0 pt-5 rounded-xl stroke-2 -mr-5 shadow-popover transform opacity-100 scale-100" id="headlessui-popover-panel-:r28:" tabindex="-1" data-headlessui-state="open">
-            <span class="user_popover_trinleShap__lPN9S"></span>
-            <div class="flex flex-col px-4">
-                <div class="flex items-center gap-3 hover:opacity-60">
+        <div className="bg-white border-wynk-borderWhite z-30 absolute top-full mt-2 w-64 h-fit dark:bg-wynk-HeaderPopDark -bottom-[152px] right-0 pt-5 rounded-xl stroke-2 -mr-5 shadow-popover transform opacity-100 scale-100" id="headlessui-popover-panel-:r28:" tabindex="-1" data-headlessui-state="open">
+            <span className="user_popover_trinleShap__lPN9S"></span>
+            <div className="flex flex-col px-4">
+                <div className="flex items-center gap-3 hover:opacity-60">
                     <span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M12 15C15.3137 15 18 12.3137 18 9C18 5.68629 15.3137 3 12 3C8.68629 3 6 5.68629 6 9C6 12.3137 8.68629 15 12 15Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M2.90625 20.2508C3.82775 18.6544 5.15328 17.3287 6.74958 16.407C8.34588 15.4853 10.1567 15 12 15C13.8433 15 15.6541 15.4853 17.2504 16.407C18.8467 17.3287 20.1722 18.6544 21.0938 20.2508" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            <path d="M12 15C15.3137 15 18 12.3137 18 9C18 5.68629 15.3137 3 12 3C8.68629 3 6 5.68629 6 9C6 12.3137 8.68629 15 12 15Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                            <path d="M2.90625 20.2508C3.82775 18.6544 5.15328 17.3287 6.74958 16.407C8.34588 15.4853 10.1567 15 12 15C13.8433 15 15.6541 15.4853 17.2504 16.407C18.8467 17.3287 20.1722 18.6544 21.0938 20.2508" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
                         </svg>
                     </span>
-                    <span class="font-light opacity-80">+918961498714</span>
+                    <span className="font-light opacity-80">Username</span>
                 </div>
             </div>
             <div>
-                <div class="flex items-center gap-3 hover:opacity-60 cursor-pointer mb-5 px-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class="inline-block" stroke="currentColor">
-                        <path d="M12 7V15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path d="M9 13L11.6464 15.6464C11.8417 15.8417 12.1583 15.8417 12.3536 15.6464L15 13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <circle cx="12" cy="12" r="9.25" stroke="currentColor" stroke-width="1.5"></circle>
+                <div className="flex items-center gap-3 hover:opacity-60 cursor-pointer mb-5 px-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className="inline-block" stroke="currentColor">
+                        <path d="M12 7V15" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <path d="M9 13L11.6464 15.6464C11.8417 15.8417 12.1583 15.8417 12.3536 15.6464L15 13" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <circle cx="12" cy="12" r="9.25" stroke="currentColor" strokeWidth="1.5"></circle>
                     </svg>
-                    <span class="font-light opacity-80">Download App</span>
+                    <span className="font-light opacity-80">Download App</span>
                 </div>
             </div>
-            <div class="flex items-center gap-3 hover:opacity-60 cursor-pointer mb-5 px-4">
+            <div className="flex items-center gap-3 hover:opacity-60 cursor-pointer mb-5 px-4">
                 <span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
                         <rect x="3.14001" y="4.4823" width="9.8" height="9.8" rx="1.5" stroke="currentColor"></rect>
                         <path d="M5.22882 12.5083L7.42751 6.25635H8.65273L10.8514 12.5083H9.79403L9.31569 11.1069H6.77294L6.2862 12.5083H5.22882ZM7.05826 10.2257H9.01358L7.90585 6.95288H8.18278L7.05826 10.2257Z" fill="currentColor"></path>
                         <rect x="10.5601" y="11.1824" width="10.8" height="10.8" rx="2" fill="#181926" stroke="currentColor"></rect>
                         <path d="M19.2 14.0623V14.7386H17.7497V20.1662H17.5495L17.0001 19.5168V17.3706C16.6509 17.4878 16.3042 17.5342 15.9649 17.5098C15.9795 17.5977 15.9893 17.6856 15.9893 17.771C15.9893 18.1397 15.8672 18.44 15.6255 18.6696C15.3813 18.8991 15.0663 19.0163 14.6781 19.0163C14.1752 19.0163 13.743 18.7916 13.3816 18.3424C13.0203 17.8931 12.8005 17.3609 12.72 16.748H12.9592C13.0423 17.2119 13.2254 17.5977 13.511 17.9029C13.7967 18.2081 14.1166 18.3619 14.473 18.3619C14.705 18.3619 14.9223 18.2862 15.1225 18.1324C15.3227 17.9786 15.4228 17.7686 15.4228 17.4976C15.4228 17.3462 15.374 17.1997 15.2788 17.0605C15.1835 16.9214 15.0444 16.8188 14.8613 16.7505C14.6781 16.6821 14.5292 16.6479 14.4096 16.6479C14.2899 16.6479 14.1092 16.6723 13.87 16.7236L13.3353 16.0985C13.9212 16.0546 14.39 15.9374 14.7465 15.747C15.103 15.559 15.2812 15.3294 15.2812 15.0633C15.2812 14.8948 15.208 14.7557 15.0639 14.6434C14.9199 14.531 14.7441 14.4773 14.5414 14.4773C14.1825 14.4773 13.7161 14.6336 13.1448 14.9486L12.72 14.3382C13.1741 14.155 13.6112 14.0623 14.0311 14.0623C14.5194 14.0623 14.915 14.1843 15.2153 14.4309C15.5156 14.6775 15.667 15.0047 15.667 15.41C15.667 15.6444 15.5913 15.8495 15.4424 16.0253C15.2934 16.2011 15.0688 16.3451 14.7709 16.4599C15.2934 16.7016 15.7256 16.8213 16.0674 16.8213C16.69 16.8213 17.0025 16.5185 17.0025 15.9105V14.7386H16.2774L15.7427 14.0623H19.2Z" fill="#E8EAED"></path>
-                        <path d="M4.56978 16.4891C4.26853 17.8974 4.64773 20.6046 8.57452 20.1673" stroke="currentColor" stroke-width="0.8"></path>
-                        <path d="M7.76624 19.2633L8.69669 20.074L7.76624 21.0635" stroke="currentColor" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path d="M3.55902 17.4042L4.36152 16.4737L5.3188 17.4042" stroke="currentColor" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M4.56978 16.4891C4.26853 17.8974 4.64773 20.6046 8.57452 20.1673" stroke="currentColor" strokeWidth="0.8"></path>
+                        <path d="M7.76624 19.2633L8.69669 20.074L7.76624 21.0635" stroke="currentColor" strokeWidth="0.8" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <path d="M3.55902 17.4042L4.36152 16.4737L5.3188 17.4042" stroke="currentColor" strokeWidth="0.8" strokeLinecap="round" strokeLinejoin="round"></path>
                     </svg>
                 </span>
-                <span class="font-light opacity-80">Select Language</span>
+                <span className="font-light opacity-80">Select Language</span>
             </div>
-            <div class="flex items-center gap-3 hover:opacity-60 cursor-pointer mb-5 px-4">
+            <div className="flex items-center gap-3 hover:opacity-60 cursor-pointer mb-5 px-4">
                 <span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                        <path d="M5.75 10.5281H3V14.4581H5.75V10.5281Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path d="M12.57 19.3181L5.75 14.4481V10.5181L12.57 5.64811C13.18 5.21811 14 5.67811 14 6.46811V18.4981C14 19.2881 13.18 19.7581 12.57 19.3181Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path d="M19.24 8.24811C20.33 9.32811 21 10.8281 21 12.4881C21 14.1481 20.33 15.6481 19.24 16.7281" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path d="M17.12 10.3681C17.66 10.9081 18 11.6581 18 12.4881C18 13.3181 17.66 14.0681 17.12 14.6081" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path d="M14 11.4881C14.55 11.4881 15 11.9381 15 12.4881C15 13.0381 14.55 13.4881 14 13.4881" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M5.75 10.5281H3V14.4581H5.75V10.5281Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <path d="M12.57 19.3181L5.75 14.4481V10.5181L12.57 5.64811C13.18 5.21811 14 5.67811 14 6.46811V18.4981C14 19.2881 13.18 19.7581 12.57 19.3181Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <path d="M19.24 8.24811C20.33 9.32811 21 10.8281 21 12.4881C21 14.1481 20.33 15.6481 19.24 16.7281" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <path d="M17.12 10.3681C17.66 10.9081 18 11.6581 18 12.4881C18 13.3181 17.66 14.0681 17.12 14.6081" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                        <path d="M14 11.4881C14.55 11.4881 15 11.9381 15 12.4881C15 13.0381 14.55 13.4881 14 13.4881" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
                     </svg>
                 </span>
-                <span class="font-light opacity-80">Sound Quality</span>
+                <span className="font-light opacity-80">Sound Quality</span>
             </div>
-            <a class="flex items-center gap-3 stroke-2 hover:opacity-60 cursor-pointer mb-5 px-4" href="/podcasts">
+            <a className="flex items-center gap-3 stroke-2 hover:opacity-60 cursor-pointer mb-5 px-4" href="/podcasts">
                 <span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                        <path d="M11.35 20.1994V20.1222L11.2753 20.1027C10.0274 19.7762 9.1 18.652 9.1 17.3044V12.0544C9.1 10.4522 10.3977 9.15444 12 9.15444C13.6023 9.15444 14.9 10.4522 14.9 12.0544V17.3044C14.9 18.652 13.9726 19.7762 12.7247 20.1027L12.65 20.1222V20.1994V21.8044C12.65 22.1617 12.3573 22.4544 12 22.4544C11.6427 22.4544 11.35 22.1617 11.35 21.8044V20.1994ZM12 18.9044C12.8802 18.9044 13.6 18.1847 13.6 17.3044V12.0544C13.6 11.1742 12.8802 10.4544 12 10.4544C11.1198 10.4544 10.4 11.1742 10.4 12.0544V17.3044C10.4 18.1847 11.1198 18.9044 12 18.9044ZM17.25 12.7044C16.8927 12.7044 16.6 12.4117 16.6 12.0544C16.6 9.51672 14.5377 7.45444 12 7.45444C9.46227 7.45444 7.4 9.51672 7.4 12.0544C7.4 12.4117 7.10727 12.7044 6.75 12.7044C6.39273 12.7044 6.1 12.4117 6.1 12.0544C6.1 8.80217 8.74773 6.15444 12 6.15444C15.2523 6.15444 17.9 8.80217 17.9 12.0544C17.9 12.4117 17.6073 12.7044 17.25 12.7044ZM20.25 12.7044C19.8927 12.7044 19.6 12.4117 19.6 12.0544C19.6 7.86672 16.1877 4.45444 12 4.45444C7.81227 4.45444 4.4 7.86672 4.4 12.0544C4.4 12.4117 4.10727 12.7044 3.75 12.7044C3.39273 12.7044 3.1 12.4117 3.1 12.0544C3.1 7.14467 7.09023 3.15444 12 3.15444C16.9098 3.15444 20.9 7.14467 20.9 12.0544C20.9 12.4117 20.6073 12.7044 20.25 12.7044Z" fill="currentColor" stroke="#191C3D" stroke-width="0"></path>
+                        <path d="M11.35 20.1994V20.1222L11.2753 20.1027C10.0274 19.7762 9.1 18.652 9.1 17.3044V12.0544C9.1 10.4522 10.3977 9.15444 12 9.15444C13.6023 9.15444 14.9 10.4522 14.9 12.0544V17.3044C14.9 18.652 13.9726 19.7762 12.7247 20.1027L12.65 20.1222V20.1994V21.8044C12.65 22.1617 12.3573 22.4544 12 22.4544C11.6427 22.4544 11.35 22.1617 11.35 21.8044V20.1994ZM12 18.9044C12.8802 18.9044 13.6 18.1847 13.6 17.3044V12.0544C13.6 11.1742 12.8802 10.4544 12 10.4544C11.1198 10.4544 10.4 11.1742 10.4 12.0544V17.3044C10.4 18.1847 11.1198 18.9044 12 18.9044ZM17.25 12.7044C16.8927 12.7044 16.6 12.4117 16.6 12.0544C16.6 9.51672 14.5377 7.45444 12 7.45444C9.46227 7.45444 7.4 9.51672 7.4 12.0544C7.4 12.4117 7.10727 12.7044 6.75 12.7044C6.39273 12.7044 6.1 12.4117 6.1 12.0544C6.1 8.80217 8.74773 6.15444 12 6.15444C15.2523 6.15444 17.9 8.80217 17.9 12.0544C17.9 12.4117 17.6073 12.7044 17.25 12.7044ZM20.25 12.7044C19.8927 12.7044 19.6 12.4117 19.6 12.0544C19.6 7.86672 16.1877 4.45444 12 4.45444C7.81227 4.45444 4.4 7.86672 4.4 12.0544C4.4 12.4117 4.10727 12.7044 3.75 12.7044C3.39273 12.7044 3.1 12.4117 3.1 12.0544C3.1 7.14467 7.09023 3.15444 12 3.15444C16.9098 3.15444 20.9 7.14467 20.9 12.0544C20.9 12.4117 20.6073 12.7044 20.25 12.7044Z" fill="currentColor" stroke="#191C3D" strokeWidth="0"></path>
                     </svg>
                 </span>
-                <span class="font-light opacity-80">Podcast</span>
+                <span className="font-light opacity-80">Podcast</span>
             </a>
             <div>
                 <div>
-                    <div class="flex items-center gap-3 hover:opacity-60 cursor-pointer mb-5 px-4">
+                    <div className="flex items-center gap-3 hover:opacity-60 cursor-pointer mb-5 px-4">
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
                                 <g clip-path="url(#clip0_604_622)">
-                                    <path d="M17.5999 20.7001L12.5999 21.9001C11.8999 22.1001 11.2999 21.6001 11.2999 20.9001V4.50013C11.2999 3.80013 11.8999 3.30013 12.5999 3.50013L17.5999 4.70013C18.0999 4.80013 18.3999 5.20013 18.3999 5.70013V19.7001C18.3999 20.1001 18.0999 20.6001 17.5999 20.7001Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                                    <path d="M9.09998 20.9001H5.59998V4.80011H9.09998" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M17.5999 20.7001L12.5999 21.9001C11.8999 22.1001 11.2999 21.6001 11.2999 20.9001V4.50013C11.2999 3.80013 11.8999 3.30013 12.5999 3.50013L17.5999 4.70013C18.0999 4.80013 18.3999 5.20013 18.3999 5.70013V19.7001C18.3999 20.1001 18.0999 20.6001 17.5999 20.7001Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                                    <path d="M9.09998 20.9001H5.59998V4.80011H9.09998" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
                                     <path d="M13.6 14.1001C14.1523 14.1001 14.6 13.4733 14.6 12.7001C14.6 11.9269 14.1523 11.3001 13.6 11.3001C13.0477 11.3001 12.6 11.9269 12.6 12.7001C12.6 13.4733 13.0477 14.1001 13.6 14.1001Z" fill="currentColor"></path>
                                 </g>
                                 <defs>
@@ -89,20 +89,20 @@ const dropdown = () => {
                                 </defs>
                             </svg>
                         </span>
-                        <span class="font-light opacity-80">Sign Out</span>
+                        <span className="font-light opacity-80">Sign Out</span>
                     </div>
                 </div>
             </div>
-            <a target="_blank" class="flex gap-3 stroke-2 hover:opacity-60 cursor-pointer mb-5 border-t border-[#2F3031] pt-5 px-4 relative" href="https://studio.wynk.in">
+            <a target="_blank" className="flex gap-3 stroke-2 hover:opacity-60 cursor-pointer mb-5 border-t border-[#2F3031] pt-5 px-4 relative" href="https://studio.wynk.in">
                 <span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                     </svg>
                 </span>
-                <div class="flex flex-col gap-1.5">
-                    <div class="flex items-center gap-[0.438rem]">
-                        <span class="font-light opacity-80 text-base">Join Wynk for Artists</span>
+                <div className="flex flex-col gap-1.5">
+                    <div className="flex items-center gap-[0.438rem]">
+                        <span className="font-light opacity-80 text-base">Join Wynk for Artists</span>
                     </div>
-                    <div class="text-xs text-wynk-gray1 pr-5">Sign up as an Artist on Wynk Studio and release your original songs on Wynk</div>
+                    <div className="text-xs text-wynk-gray1 pr-5">Sign up as an Artist on Wynk Studio and release your original songs on Wynk</div>
                 </div>
             </a>
         </div>
@@ -112,4 +112,4 @@ const dropdown = () => {
 )
 }
 
-export default dropdown
+export default Dropdown
