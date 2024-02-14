7cebd5b428b38b97b72faf07bf681531bf46635c
Bishal Prasad <bishal.prasad3787@gmail.com>
2024-02-14 18:28:45 +0530

    Added Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'; // Import useState from React
+import React, { useState } from 'react'; 
+import ProtectedRoute from './components/Authentication/ProtectedRoute'; 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer/Footer'
-import TrendingSongs from './components/Songs/TrendingSongs';
 import Playlist from './components/Common/Playlist';
 import Subscription from './components/Subscription/Subscription';
 import LoginModal from './components/Authentication/LoginSignupModal';
+import MyMusic from './components/Music/MyMusic';
 
 const App = () => {
   const [currentSong, setCurrentSong] = useState(null);
@@ -16,10 +17,10 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/playlist" element={<Playlist setCurrentSong={setCurrentSong} />} />
-        <Route path="/trending" element={<TrendingSongs />} />
-        <Route path="/subscription" element={<Subscription />} />
         <Route path="/Signup" element={<LoginModal />} />
+        <Route path="/subscription" element={<Subscription />} />
+        <Route path="/playlist" element={<Playlist setCurrentSong={setCurrentSong} />} />
+        <Route path="/mymusic" element={<ProtectedRoute component={MyMusic} />} />
       </Routes>
       <Footer />
     </Router>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,6 @@ import { IoMenu } from "react-icons/io5";
 import LoginModal from "../Authentication/LoginSignupModal";
 import Dropdown from "./Dropdown";
 import { useUser } from "../../utils/UserProvider";
-import { MyLibrary } from "../../pages/MyLibrary";
 
 const Navbar = () => {
     const [showLogin, setShowLogin] = useState(false);
@@ -37,6 +36,11 @@ const Navbar = () => {
         else setShowLogin(true);
     };
 
+    const handleMyMusicClick = () => {
+        if (isUserLoggedIn) navigate('/mymusic');
+        else setShowLogin(true);
+    };
+
     return (
         <div className={`${shownavbar ? 'block' : 'hidden'}`}>
             <nav className='h-[70px] w-full bg-[#1A1A1A] grid grid-cols-1 lg:grid-cols-3 gap-3'>
@@ -50,18 +54,41 @@ const Navbar = () => {
                         <input type='text' placeholder='Search Songs' className='bg-transparent focus:outline-none text-slate-400 text-xs lg:text-sm lg:w-40' />
                     </div>
                     <div className='flex items-center justify-center gap-2 lg:gap-3 ml-4'>
-                        <button className='text-white flex items-center h-8 lg:h-10 gap-1 text-xs lg:text-base' onClick={handleSubscriptionClick}>
-                            <BsCurrencyRupee className='h-4 lg:h-6 w-4 lg:w-6' />
-                            Manage Subscription
-                        </button>
+                        <span className="hover:opacity-60 cursor-pointer flex" onClick={handleSubscriptionClick}>
+                            <span className="text-white hidden lg:block">
+                                <BsCurrencyRupee className='h-4 lg:h-6 w-4 lg:w-6' />
+                            </span>
+                            <div className="text-white hidden lg:block ml-2 font-light">Manage Subscription</div>
+                        </span>
                         {isUserLoggedIn === true ? (
-                            <MyLibrary />
+                            <>
+                                <RxDividerVertical className='text-white h-7 lg:h-10 w-7 lg:w-10' />
+                                <span className="hover:opacity-60 cursor-pointer flex" onClick={handleMyMusicClick}>
+                                    <span className="text-white hidden lg:block">
+                                        <span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                                                <path d="M6.37001 21.5383C7.77833 21.5383 8.92001 20.4011 8.92001 18.9983C8.92001 17.5954 7.77833 16.4583 6.37001 16.4583C4.96168 16.4583 3.82001 17.5954 3.82001 18.9983C3.82001 20.4011 4.96168 21.5383 6.37001 21.5383Z"
+                                                    stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+                                                </path>
+                                                <path d="M8.91003 18.9982V6.57825" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                <path d="M17.63 17.9982C19.0383 17.9982 20.18 16.861 20.18 15.4582C20.18 14.0554 19.0383 12.9182 17.63 12.9182C16.2216 12.9182 15.08 14.0554 15.08 15.4582C15.08 16.861 16.2216 17.9982 17.63 17.9982Z"
+                                                    stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+                                                </path>
+                                                <path d="M20.1801 15.4582V3.03821" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                <path d="M8.91003 6.57821L20.18 3.03821" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                <path d="M9.22998 10.4181L20.18 6.97815" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                                            </svg>
+                                        </span>
+                                    </span>
+                                    <div className="text-white hidden lg:block ml-2 font-light">My Music</div>
+                                </span>
+                            </>
                         ) : (
                             <>
                                 <RxDividerVertical className='text-white h-7 lg:h-10 w-7 lg:w-10' />
-                                <button className='text-white flex items-center h-8 lg:h-10 gap-1' onClick={handleShowLogin}>
+                                <button className='hover:opacity-60 cursor-pointer text-white flex items-center h-8 lg:h-10 gap-1' onClick={handleShowLogin}>
                                     <FaRegUser className='h-4 lg:h-5 w-4 lg:w-5' />
-                                    Login
+                                    <div className="text-white hidden lg:block ml-2 font-light">Login</div>
                                 </button>
                             </>
                         )}
