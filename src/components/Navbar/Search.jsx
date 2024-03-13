ec056a7e3f0da26815e6eecd85675245244f8a2b
Bishal Prasad <bishal.prasad3787@gmail.com>
2024-03-13 21:21:07 +0530

    Implemented Searches

--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -1,29 +1,37 @@
 import React, { useEffect } from 'react';
 import { useUser } from '../../utils/UserProvider';
+import MusicPlayer from '../Music/MusicPlayer';
 
 const Search = () => {
-
+    const { setCurrentSong, currentSong } = useUser();
     const { searchData, setsearchData } = useUser();
     console.log(searchData);
-    useEffect(() => {
 
+    useEffect(() => {
     }, [searchData])
 
     const handleClickSong = (song) => {
-
+        setCurrentSong(song);
     };
 
     return (
-        <div>
-            <h2 className="text-left mx-20 my-10 text-xl text-white">Search Songs</h2>
-            <div className='mx-8 px-10'>
-                {searchData.map((song) => (
-                    <div key={song._id} className='bg-red-200 h-[160px] w-[130px] rounded-[40px]' onClick={() => handleClickSong(song)}>
-                        <img className='rounded-md h-full w-full' src={song.thumbnail} alt={song.title} />
-                        <h4 className='text-white truncate p-2'>{song.title}</h4>
-                    </div>
-                ))}
+        <div className="h-full">
+            <h2 className="text-left mx-20 my-10 text-xl text-white">Recent Searches</h2>
+            <div className='mx-20 my-10'>
+                <div className="flex-shrink-0 mt-5 gap-2">
+                    {searchData.map((song) => (
+                        <div key={song._id} className="inline-block sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-5" onClick={() => handleClickSong(song)}>
+                            <a title={song.title} className="rounded-xl">
+                                <div className="rounded-xl">
+                                    <img src={song.thumbnail} className="w-44 h-44 rounded-xl" />
+                                </div>
+                                <div className="truncate font-normal w-44 text-white text-base text-left pt-2">{song.title}</div>
+                            </a>
+                        </div>
+                    ))}
+                </div>
             </div>
+            {currentSong && <MusicPlayer />}
         </div>
     );
 };
