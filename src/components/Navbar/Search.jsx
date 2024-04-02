a5020e604f1016f13ecc436374f44a391d60b5af
Bishal Prasad <bishal.prasad3787@gmail.com>
2024-04-02 22:36:11 +0530

    Fixed Bugs and Svg

--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -3,12 +3,10 @@ import { useUser } from '../../utils/UserProvider';
 import MusicPlayer from '../Music/MusicPlayer';
 
 const Search = () => {
-    const { setCurrentSong, currentSong } = useUser();
-    const { searchData, setsearchData } = useUser();
-    console.log(searchData);
+    const { setCurrentSong, currentSong, searchData, setsearchData } = useUser();
 
     useEffect(() => {
-    }, [searchData])
+    }, [searchData]);
 
     const handleClickSong = (song) => {
         setCurrentSong(song);
@@ -19,11 +17,11 @@ const Search = () => {
             <h2 className="text-left mx-20 my-10 text-xl text-white">Recent Searches</h2>
             <div className='mx-20 my-10'>
                 <div className="flex-shrink-0 mt-5 gap-2">
-                    {searchData.map((song) => (
+                    {searchData && searchData.map((song) => (
                         <div key={song._id} className="inline-block sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-5" onClick={() => handleClickSong(song)}>
                             <a title={song.title} className="rounded-xl">
                                 <div className="rounded-xl">
-                                    <img src={song.thumbnail} className="w-44 h-44 rounded-xl" />
+                                    <img src={song.thumbnail} className="w-44 h-44 rounded-xl" alt={song.title} />
                                 </div>
                                 <div className="truncate font-normal w-44 text-white text-base text-left pt-2">{song.title}</div>
                             </a>
