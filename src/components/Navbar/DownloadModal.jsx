06cb181e01398af7cea1bb234ee9da1413faecb1
Bishal Prasad <bishal.prasad3787@gmail.com>
2024-04-03 09:57:29 +0530

    Updated Svg and fixed Bugs

--- src/components/Navbar/DownloadModal.jsx
+++ src/components/Navbar/DownloadModal.jsx
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import { Modal } from '@mui/material';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import download from "../../assets/images/Download.JPG";
-import asImg from "../../assets/images/AppStore.png"
-import psImg from '../../assets/images/PlayStore.png'
+import downloadImage from "../../assets/images/Download.JPG";
+import appStoreImage from "../../assets/images/AppStore.png";
+import playStoreImage from '../../assets/images/PlayStore.png';
 
 const DownloadModal = ({ showDownloadModal, handleClose }) => {
     const [email, setEmail] = useState('');
 
     const handleSubmit = () => {
         try {
+            if (email.trim() === '') {
+                toast.error('Please enter your email address.', { autoClose: 2000 });
+                return;
+            }
             toast.success('Link Sent successfully!', { autoClose: 1000 });
             handleClose();
         } catch (error) {
@@ -21,12 +25,13 @@ const DownloadModal = ({ showDownloadModal, handleClose }) => {
     return (
         <Modal
             open={showDownloadModal}
+            onClose={() => {}}            
             aria-labelledby="Credential Modal"
             style={{ backdropFilter: "blur(5px)" }}
         >
             <div className="h-[450px] w-[320px] lg:w-[750px] grid grid-cols-1 lg:grid-cols-5 bg-black absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-xl">
                 <div className='lg:col-span-2'>
-                    <img src={download} alt='Login?SignUpImage' className='h-[450px] w-full lg:rounded-tl-xl lg:rounded-bl-xl' />
+                    <img src={downloadImage} alt='Download' className='h-[450px] w-full lg:rounded-tl-xl lg:rounded-bl-xl' />
                 </div>
                 <div className='lg:col-span-3 p-5 flex flex-col items-center justify-around'>
                     <h1 className='text-white text-5xl w-full '>Take Wynk</h1>
@@ -38,8 +43,8 @@ const DownloadModal = ({ showDownloadModal, handleClose }) => {
                     </div>
                     <div className='flex items-center justify-center'>
                         <p className='text-white text-xs lg:text-sm'>Available on</p>
-                        <img src={asImg} alt='as' className='h-10 lg:h-14 w-24 lg:w-32' />
-                        <img src={psImg} alt='ps' className='h-10 lg:h-14 w-24 lg:w-32' />
+                        <img src={appStoreImage} alt='App Store' className='h-10 lg:h-14 w-24 lg:w-32' />
+                        <img src={playStoreImage} alt='Play Store' className='h-10 lg:h-14 w-24 lg:w-32' />
                     </div>
                 </div>
             </div>
