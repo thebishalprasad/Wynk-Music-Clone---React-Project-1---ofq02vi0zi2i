8144f4f184fe33723549c9fa9826e1b7562fbbaa
Bishal Prasad <bishal.prasad3787@gmail.com>
2024-05-18 21:18:09 +0530

    Updated Login UI

--- src/components/Authentication/LoginSignupModal.jsx
+++ src/components/Authentication/LoginSignupModal.jsx
@@ -62,14 +62,14 @@ const LoginModal = ({ showLogin, handleClose, navigate }) => {
             open={showLogin}
             onClose={handleClose}
             aria-labelledby="Credential Modal"
-            style={{ backdropFilter: "blur(5px)" }}
+            style={{ backdropFilter: "blur(18px)" }}
         >
-            <div className="h-[450px] w-[320px] lg:w-[750px] grid grid-cols-1 lg:grid-cols-5 bg-black absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-xl">
+            <div className="h-[450px] w-[320px] lg:w-[700px] grid grid-cols-1 lg:grid-cols-5 bg-black absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] rounded-xl">
                 <div className='lg:col-span-2'>
                     <img src={loginImg} alt='Login?SignUpImage' className='h-[450px] w-full lg:rounded-tl-xl lg:rounded-bl-xl' />
                 </div>
                 <div className='lg:col-span-3 p-5 flex flex-col items-center justify-around'>
-                    <h1 className='text-white text-3xl w-full'>{isLogin ? 'Login' : 'Sign Up'}</h1>
+                    <h1 className='text-white font-bold text-3xl w-full'>{isLogin ? 'Login' : 'Sign Up'}</h1>
                     <p className='text-white text-sm lg:text-base'>Get a personalized experience and access all your music</p>
 
                     {!isLogin && <input type='text' placeholder='Username' value={name} onChange={(e) => setName(e.target.value)} className='text-white bg-[#1b1b1c] focus:outline-none px-2 h-10 rounded-md w-full' />}
@@ -79,10 +79,10 @@ const LoginModal = ({ showLogin, handleClose, navigate }) => {
                         <button className='bg-white h-10 w-24 lg:w-40 rounded-md text-xs lg:text-base' onClick={handleSubmit}>{isLogin ? 'Login' : 'Sign Up'}</button>
                         <button className='bg-white h-10 w-24 lg:w-40 rounded-md text-xs lg:text-base' onClick={() => setIsLogin(!isLogin)}>{isLogin ? 'Sign Up' : 'Login'}</button>
                     </div>
-                    <div className='flex items-center justify-center'>
+                    <div className='flex items-center justify-center gap-3'>
                         <p className='text-white text-xs lg:text-sm'>Available on</p>
-                        <img src={asImg} alt='as' className='h-10 lg:h-14 w-24 lg:w-32' />
-                        <img src={psImg} alt='ps' className='h-10 lg:h-14 w-24 lg:w-32' />
+                        <img src={psImg} alt='ps' className='h-10 lg:h-14 w-24 lg:w-28' />
+                        <img src={asImg} alt='as' className='h-10 lg:h-14 w-24 lg:w-28' />
                     </div>
                 </div>
             </div>
