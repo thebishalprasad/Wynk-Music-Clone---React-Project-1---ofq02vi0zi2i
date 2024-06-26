0237e4a0c7f212b53a1e15e43b4e4562c4edef61
Bishal Prasad <bishal.prasad3787@gmail.com>
2024-06-26 19:15:41 +0530

    Updated UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ "./public/index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  "./src/**/*.{js,ts,jsx,tsx,html}",],
   theme: {
     extend: {
       screens: {
-        'xs': '400px', // Custom breakpoint for extra small screens
+        'xs': '320px', 
       },
     },
   },
